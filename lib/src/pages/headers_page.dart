fa691a75a701671c2803a6a474160b57748e7496
ddienu <rockdann@hotmail.com>
2022-09-22 21:57:41 -0500

    Update: Main, add: Headers page, add: Headers Widget
    Creating the designs app with the headers page and headers widget with a container.

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:disenos_app/src/widgets/headers_widget.dart';
+
+
+
+class HeadersPage extends StatelessWidget {
+  const HeadersPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HeaderCuadrado(),
+    );
+  }
+}
---
+++ lib/src/widgets/headers_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 220.0,
+      color: Color(0xFF615AAB),
+    );
+  }
+}
