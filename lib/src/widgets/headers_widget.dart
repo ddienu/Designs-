e80fe6691b0d7f43bdf2d0ec8383d46e836d2a4d
ddienu <rockdann@hotmail.com>
2022-09-22 22:27:25 -0500

    Update: headers page, update: headers widget
    Update headers widget with a new class called HeaderBordesRedondeados to create a headers with the bottom left and right with a border radius of 50.
    Update headers page implementing the class created in the headers widget.

--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -12,3 +12,18 @@ class HeaderCuadrado extends StatelessWidget {
     );
   }
 }
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 220.0,
+      decoration: BoxDecoration(
+        color: Color(0xFF615AAB),
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50.0), bottomRight: Radius.circular(50.0) ),
+      ),
+    );
+  }
+}
