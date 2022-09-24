e7dd2127ea2719c7e84a2cba4b58224505a9ac6c
ddienu <rockdann@hotmail.com>
2022-09-24 00:33:36 -0500

    Update: headers widget, update: headers page
    Update headers widget with a new method called HeaderDiagonal and a new class that extends Custom Painter in it implements an instance of Paint and path to draw the custom header with the custom painter.
    Update headers page implementing the HeaderDiagonal into the body of the Scaffold

--- lib/src/widgets/headers_widget.dart
+++ lib/src/widgets/headers_widget.dart
@@ -22,8 +22,57 @@ class HeaderBordesRedondeados extends StatelessWidget {
       height: 220.0,
       decoration: BoxDecoration(
         color: Color(0xFF615AAB),
-        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(50.0), bottomRight: Radius.circular(50.0) ),
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50.0), 
+          bottomRight: Radius.circular(50.0) 
+          ),
       ),
     );
   }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _HeaderDiagonalPainter(),
+      ),
+
+    );
+  }
+}
+
+class _HeaderDiagonalPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    final paint = Paint();
+
+    //Properties
+    paint.color = Color(0xFF615AAB);
+    paint.style = PaintingStyle.fill;
+    //paint.strokeWidth = 10.0;
+
+    final path = Path();
+
+    //Draw with the path and paint.
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height * 0.5);
+    path.lineTo(size.width, size.height * 0.3);
+    path.lineTo(size.width, 0);
+
+    canvas.drawPath(path, paint);  
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+      return true;
+  }
+
+
 }
