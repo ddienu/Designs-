29507f1db4fb0e5eb2b587c425df7c0f9966405d
ddienu <rockdann@hotmail.com>
2022-10-02 22:22:11 -0500

    Update: circular progress page.
    Update circular progress page with CustomPaint in the child of the container with the purpose to draw the circle of the circular progress indicator also draw an arc that will show the progress in our circle.

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dart:math';
+
+
 
 class CircularProgressPage extends StatefulWidget {
   const CircularProgressPage({Key? key}) : super(key: key);
@@ -14,11 +17,62 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          padding: EdgeInsets.all(4.0),
           height: 300.0,
           width: 300.0,
-          color: Colors.red,
+          //color: Colors.red,
+          child: CustomPaint(
+            painter: _MiRadialProgress( 50 ),
+          ),
         ),
       ),
     );
   }
+}
+
+class _MiRadialProgress extends CustomPainter {
+
+  final porcentaje;
+
+  _MiRadialProgress( this.porcentaje );
+  @override
+  void paint(Canvas canvas, Size size) {
+
+    //Draw full circle.
+
+    final paint = Paint();
+
+    paint.color = Colors.grey;
+    paint.style = PaintingStyle.stroke;
+    paint.strokeWidth = 4.0;
+
+    final  center = Offset(size.width * 0.5, size.height* 0.5);
+    final  radio  = min(size.width * 0.5, size.height * 0.5);
+
+    canvas.drawCircle(center, radio, paint);
+
+    //Draw arc.
+
+    final paintArco = Paint()
+    ..color = Colors.pink
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10.0;
+
+    final arcAngle = 2 * pi * (porcentaje / 100);
+
+    canvas.drawArc(
+      Rect.fromCircle(center: center, radius: radio), 
+      -pi / 2, 
+      arcAngle, 
+      false, 
+      paintArco);
+
+    
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return true;
+  }
+
 }
