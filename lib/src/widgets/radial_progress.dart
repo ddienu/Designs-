bff487da18f24f0e4f097ede337b8fcf77360aa6
ddienu <rockdann@hotmail.com>
2022-10-05 01:29:38 -0500

    Update: radial progress.
    Update radial progress implemeting in the state the singletickerprovider to make the animation, add two new properties, one the animation controller and the other one a double called porcentajeAnterior that will save the initial percent, include the init state initializing the controller with a duration of 200 milliseconds and the dispose.
    In the build method implementing the controller forward from 0.0 and create a new property called diferenciaAnimar that is the difference between widget.porcentaje and porcentajeAnterior also implementing a AnimatedBuilder that return the container with the custom paint.

--- lib/src/widgets/radial_progress.dart
+++ lib/src/widgets/radial_progress.dart
@@ -12,22 +12,52 @@ class RadialProgress extends StatefulWidget {
   State<RadialProgress> createState() => _RadialProgressState();
 }
 
-class _RadialProgressState extends State<RadialProgress> {
+class _RadialProgressState extends State<RadialProgress>  with SingleTickerProviderStateMixin{
+
+  late AnimationController controller;
+  late double porcentajeAnterior;
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(10.0),
-      width: double.infinity,
-      height: double.infinity,
-      child: CustomPaint(
-        painter: _MiRadialProgress( widget.porcentaje ),
-      ),
-      
-    );
+  void initState() {
     
+    porcentajeAnterior = widget.porcentaje;
+
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 200)
+    );
+    super.initState();
   }
 
-  
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    controller.forward( from: 0.0 );
+
+    final diferenciaAnimar = widget.porcentaje - porcentajeAnterior;
+
+    porcentajeAnterior = widget.porcentaje;
+
+    return AnimatedBuilder(
+      animation: controller, 
+      builder: (BuildContext context, Widget? child){
+        return Container(
+          padding: EdgeInsets.all(10.0),
+          width: double.infinity,
+          height: double.infinity,
+          child: CustomPaint(
+            painter: _MiRadialProgress( (widget.porcentaje - diferenciaAnimar) + ( diferenciaAnimar * controller.value) ),
+          ),
+          
+        );
+      }
+      );
+    
+  }
 }
 class _MiRadialProgress extends CustomPainter {
 
