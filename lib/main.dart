2b10fff0da16ea40d45ca801086bae61d54f31e6
ddienu <rockdann@hotmail.com>
2022-10-07 22:50:11 -0500

    Update: slideshow page, update: pubspec.
    Update slideshow page using the svgpicture.asset.
    Update pubspec.yaml adding the assets into the bundle and installing the flutter svg package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:disenos_app/src/pages/slideshow_page.dart';
 import 'package:flutter/material.dart';
 
-import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
+import 'package:disenos_app/src/pages/slideshow_page.dart';
+
+//import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
 // import 'package:disenos_app/src/pages/headers_page.dart';
 // import 'package:disenos_app/src/retos/cuadrado_animado_page.dart';
 
--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 
 class SlideShowPage extends StatelessWidget {
   const SlideShowPage({Key? key}) : super(key: key);
 
+  final assetName = 'assets/svgs/slide-1.svg';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Hola Mundo'),
+        child: SvgPicture.asset(
+          assetName
+          ),
       ),
     );
+      
   }
 }
