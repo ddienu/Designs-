4ddb8a4fd6bfba9dd6c3e61b042b343e13052862
ddienu <rockdann@hotmail.com>
2022-10-07 23:10:33 -0500

    Update: slideshow page.
    Update the slideshow page extracting the container widget and name it _Slides using a pageview and in its children  the svgs images, also extract the svgpicture widget to have better control to the size of the image with a container, in this new widget called _Slide using a property of String called svg and using it as a constructor and as an argument.

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -5,17 +5,49 @@ import 'package:flutter_svg/flutter_svg.dart';
 class SlideShowPage extends StatelessWidget {
   const SlideShowPage({Key? key}) : super(key: key);
 
-  final assetName = 'assets/svgs/slide-1.svg';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: SvgPicture.asset(
-          assetName
-          ),
+        child: _Slides(),
       ),
     );
       
+  }
+}
+
+class _Slides extends StatelessWidget {
+  const _Slides({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          const _Slide( 'assets/svgs/slide-1.svg' ),
+          const _Slide( 'assets/svgs/slide-2.svg' ),
+          const _Slide( 'assets/svgs/slide-3.svg' ),
+          
+        ],
+      ),
+    );
+    
+  }
+}
+
+class _Slide extends StatelessWidget {
+
+  final String svg;
+  
+  const _Slide (this.svg);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding : EdgeInsets.all(30.0),
+      child: SvgPicture.asset( svg ),
+    );
   }
 }
