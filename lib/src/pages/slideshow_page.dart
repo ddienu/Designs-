1864febc212c0cd6d7f871eb9b4794a0c39db271
ddienu <rockdann@hotmail.com>
2022-10-13 21:16:53 -0500

    Update: Slideshow page
    Update Slideshow page adding a column to put two slideshows in the same page as the widget is totally customizable, the user can change all as its like.

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:disenos_app/src/widgets/slideshow.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -10,19 +12,38 @@ class SlideshowPage extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return Scaffold(
-      body: Slideshow(
-        bulletPrimario: 15.0,
-        bulletSecundario: 12.0,
-        colorPrimario: Colors.grey,
-        colorSecundario: Colors.pink,
-        puntosArriba: false,
-        slides: [
-        SvgPicture.asset('assets/svgs/slide-1.svg' ),
-        SvgPicture.asset('assets/svgs/slide-2.svg' ),
-        SvgPicture.asset('assets/svgs/slide-3.svg' ),
-        SvgPicture.asset('assets/svgs/slide-4.svg' ),
-        SvgPicture.asset('assets/svgs/slide-5.svg' ),
-      ],),
+      body: Column(
+        children: [
+          Expanded(
+            child: Slideshow(
+              bulletPrimario: 15.0,
+              bulletSecundario: 12.0,
+              colorPrimario: Colors.grey,
+              colorSecundario: Colors.pink,
+              puntosArriba: false,
+              slides: [
+              SvgPicture.asset('assets/svgs/slide-1.svg' ),
+              SvgPicture.asset('assets/svgs/slide-2.svg' ),
+              SvgPicture.asset('assets/svgs/slide-3.svg' ),
+              SvgPicture.asset('assets/svgs/slide-4.svg' ),
+              SvgPicture.asset('assets/svgs/slide-5.svg' ),
+            ],
+          ),
+        ),
+          Expanded(
+            child: Slideshow(
+              bulletPrimario: 15.0,
+              bulletSecundario: 12.0,
+              colorPrimario: Colors.grey,
+              colorSecundario: Colors.black,
+              puntosArriba: true,
+              slides: [
+              Image.network('https://i.ytimg.com/vi/tshXzKpgZWs/hqdefault.jpg'),
+              Center(child: Text('Table Tennis 4 All', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),)),
+              Image.network('https://tabletennis-reference.com/images/racket/935_1_450.jpg'),
+              ],))
+        ],
+      ),
     );
     
   }
