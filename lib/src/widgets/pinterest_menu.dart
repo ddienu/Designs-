d58efbb9170ace337d7fb3fb078812a95b47d02d
ddienu <rockdann@hotmail.com>
2022-10-15 23:41:46 -0500

    Update: pinterest menu.
    Update pinterest menu customizing the icon of the _PinterestMenuButton, adding a color and size, also extracting the container of the PinterestMenu to optimize the code.

--- lib/src/widgets/pinterest_menu.dart
+++ lib/src/widgets/pinterest_menu.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 
 class PinterestButton {
 
-  final Function onPressed;
+  final Function() onPressed;
   final IconData icon;
 
 
@@ -22,29 +22,45 @@ class PinterestMenu extends StatelessWidget {
 
     final List<PinterestButton> items = [
 
-      PinterestButton(icon: Icons.pie_chart, onPressed: (){ print({'$Icon.name'});}),
-      PinterestButton(icon: Icons.search, onPressed: (){ print({'$Icon.name'});}),
-      PinterestButton(icon: Icons.notifications, onPressed: (){ print({'$Icon.name'});}),
-      PinterestButton(icon: Icons.supervised_user_circle, onPressed: (){ print({'$Icon.name'});}),
+      PinterestButton(icon: Icons.pie_chart, onPressed: (){ print({'Icon pie_chart'});}),
+      PinterestButton(icon: Icons.search, onPressed: (){ print({'Icon search'});}),
+      PinterestButton(icon: Icons.notifications, onPressed: (){ print({'Icon notifications'});}),
+      PinterestButton(icon: Icons.supervised_user_circle, onPressed: (){ print({'Icon supervised'});}),
 
     ];
 
     return Center(
-      child: Container(
-        child: _MenuItems( menuItems: items),
-        width: 250.0,
-        height: 60.0,
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(50.0),
-          boxShadow: <BoxShadow>[
-            BoxShadow(
-              color: Colors.black38,
-              blurRadius: 10.0,
-              spreadRadius: -5,
-            )
-          ]
-        ),
+      child: _PinterestMenuBackground(child: _MenuItems( menuItems: items)),
+    );
+  }
+}
+
+class _PinterestMenuBackground extends StatelessWidget {
+
+  final Widget child; 
+
+  const _PinterestMenuBackground({
+
+    required this.child,
+
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+      width: 250.0,
+      height: 60.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(50.0),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black38,
+            blurRadius: 10.0,
+            spreadRadius: -5,
+          )
+        ]
       ),
     );
   }
@@ -76,8 +92,16 @@ class _PinterestMenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Icon( item.icon ),
+    return GestureDetector(
+      onTap: item.onPressed,
+      behavior: HitTestBehavior.translucent,
+      child: Container(
+        child: Icon( 
+          item.icon, 
+          color: Colors.blueGrey,
+          size: 25.0,
+          ),
+      ),
     );
   }
 }
