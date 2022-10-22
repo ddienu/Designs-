9bb0367aab29ee4e55911a9708d6c7f837248592
ddienu <rockdann@hotmail.com>
2022-10-22 01:59:23 -0500

    Update: emergency page.
    Update emergency page extracting the Boton Gordo widget in a new widget called BotonGordoTemp, also in the scaffold implement a stack to put all the boton gordo widget and the iconheader in the same page.
    Implement a class called ItemBoton with the properties and the costructor implementing those properties, inside the build method put a list of the class ItemBoton, with the properties needed in the class ItemBoton, to turn the list in an instance of BotonGordo, use the map ah convert it to list.
    Implement the map with the spread operator into the listview to create the buttons automatically.

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -4,6 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
+class ItemBoton {
+
+  final IconData icon;
+  final String texto;
+  final Color color1;
+  final Color color2;
+
+  ItemBoton( this.icon, this.texto, this.color1, this.color2 );
+}
 
 class EmergencyPage extends StatelessWidget {
   const EmergencyPage({Key? key}) : super(key: key);
@@ -11,20 +20,73 @@ class EmergencyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final items = <ItemBoton>[
+  ItemBoton( FontAwesomeIcons.carBurst, 'Motor Accident', const Color(0xff6989F5), const Color(0xff906EF5) ),
+  ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', const Color(0xff66A9F2), const Color(0xff536CF6) ),
+  ItemBoton( FontAwesomeIcons.masksTheater, 'Theft / Harrasement', const Color(0xffF2D572), const Color(0xffE06AA3) ),
+  ItemBoton( FontAwesomeIcons.personBiking, 'Awards', const Color(0xff317183), const Color(0xff46997D) ),
+  ItemBoton( FontAwesomeIcons.carBurst, 'Motor Accident', const Color(0xff6989F5), const Color(0xff906EF5) ),
+  ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', const Color(0xff66A9F2), const Color(0xff536CF6) ),
+  ItemBoton( FontAwesomeIcons.masksTheater, 'Theft / Harrasement', const Color(0xffF2D572), const Color(0xffE06AA3) ),
+  ItemBoton( FontAwesomeIcons.personBiking, 'Awards', const Color(0xff317183), const Color(0xff46997D) ),
+  ItemBoton( FontAwesomeIcons.carBurst, 'Motor Accident', const Color(0xff6989F5), const Color(0xff906EF5) ),
+  ItemBoton( FontAwesomeIcons.plus, 'Medical Emergency', const Color(0xff66A9F2), const Color(0xff536CF6) ),
+  ItemBoton( FontAwesomeIcons.masksTheater, 'Theft / Harrasement', const Color(0xffF2D572), const Color(0xffE06AA3) ),
+  ItemBoton( FontAwesomeIcons.personBiking, 'Awards', const Color(0xff317183), const Color(0xff46997D) ),
+];
+
+  List<Widget> itemMap = items.map(
+    (item) => BotonGordo(
+      icono: item.icon,
+      titulo: item.texto,
+      color1: item.color1,
+      color2: item.color2,
+      onPressed: () { print('Hola');},
+    )).toList();
+
     return Scaffold(
-      body: Center(
-        child: BotonGordo(
-          icono: FontAwesomeIcons.plus,
-          iconoBoton: FontAwesomeIcons.chevronRight,
-          titulo: 'Medical Emergency',
-          color1: Colors.blueAccent,
-          color2: Colors.blue,
-          onPressed: (){},
-        ),
+      body: Stack(
+        children: [
+          Container(
+            margin: EdgeInsets.only( top: 200.0),
+            child: ListView(
+               children: [
+               SizedBox(height: 110.0),
+               BotonGordoTemp(),
+               ...itemMap
+               ]
+              ),
+          ),
+          IconHeader(
+             icon: FontAwesomeIcons.plus, 
+             titulo: 'Haz solicitado', 
+             subtitulo: 'Emergencia médica',
+             color1: const Color(0xff536CF6),
+             color2: const Color(0xff66A9F2),
+             ),
+        ],
       )
-          );
+      );
     }
   }
+
+class BotonGordoTemp extends StatelessWidget {
+  const BotonGordoTemp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BotonGordo(
+      icono: FontAwesomeIcons.plus,
+      iconoBoton: FontAwesomeIcons.chevronRight,
+      titulo: 'Medical Emergency',
+      color1: Colors.blueAccent,
+      color2: Colors.blue,
+      onPressed: (){},
+    );
+  }
+}
 
 class PageHeader extends StatelessWidget {
   const PageHeader({
