4d9dbe35ebf6625123e1353966a78e83680b2784
ddienu <rockdann@hotmail.com>
2022-10-24 22:55:57 -0500

    Add: Sliver list page, update: main
    Add Sliver list page with a stateless widget that returns a Scaffold to prepare the page to start code.
    Update main implementing the new page created.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-import 'package:disenos_app/src/pages/emergency_page.dart';
+import 'package:disenos_app/src/pages/sliver_list_page.dart';
 
-
+//import 'package:disenos_app/src/pages/emergency_page.dart';
 //import 'package:disenos_app/src/labs/slideshow_page.dart';
 //import 'package:disenos_app/src/pages/graficas_circulares_page.dart';
 // import 'package:disenos_app/src/pages/headers_page.dart';
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Designs App',
-      home: EmergencyPage(),
+      home: SliverListPage(),
     );
   }
 }
---
+++ lib/src/pages/sliver_list_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SliverListPage extends StatelessWidget {
+  const SliverListPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: Center(
+        child: Text('Hola mundo'))
+    );
+    
+  }
+}
