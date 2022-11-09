73a3f65a6aa8aeb0b99b8aa0d6cd6b2231ee468f
ddienu <rockdann@hotmail.com>
2022-11-08 23:52:52 -0500

    Update: slideshow page.
    Update slideshow page extracting the Slideshow widgets to make the code more understandable, also adding a bool property called isLarge to know when the device has more than 500 pixels in height, if isLarge is true will return a Column if not will return a row to give to the app a better looking in a device with less than 500 pixels of height.

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -16,40 +16,84 @@ class SlideshowPage extends StatelessWidget {
     final appTheme    = Provider.of<ThemeChanger>(context);
     final accentColor = appTheme.currentTheme.colorScheme.secondary;
 
-    return Scaffold(
-      body: Column(
-        children: [
+    // bool isLarge;
+    // if ( MediaQuery.of(context).size.height > 500){
+    //   isLarge = true;
+    // }else{
+    //   isLarge = false;
+    // }
+
+    bool isLarge = (MediaQuery.of(context).size.height > 500);
+
+    final children = [
           Expanded(
-            child: Slideshow(
-              bulletPrimario: 15.0,
-              bulletSecundario: 12.0,
-              colorPrimario: Colors.grey,
-              colorSecundario: accentColor,
-              puntosArriba: false,
-              slides: [
-              SvgPicture.asset('assets/svgs/slide-1.svg' ),
-              SvgPicture.asset('assets/svgs/slide-2.svg' ),
-              SvgPicture.asset('assets/svgs/slide-3.svg' ),
-              SvgPicture.asset('assets/svgs/slide-4.svg' ),
-              SvgPicture.asset('assets/svgs/slide-5.svg' ),
-            ],
-          ),
+            child: _MiSlideShow1(accentColor: accentColor),
         ),
           Expanded(
-            child: Slideshow(
-              bulletPrimario: 15.0,
-              bulletSecundario: 12.0,
-              colorPrimario: Colors.grey,
-              colorSecundario: accentColor,
-              puntosArriba: true,
-              slides: [
-              Image.network('https://i.ytimg.com/vi/tshXzKpgZWs/hqdefault.jpg'),
-              Center(child: Text('Table Tennis 4 All', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),)),
-              Image.network('https://tabletennis-reference.com/images/racket/935_1_450.jpg'),
-              ],))
-        ],
-      ),
+            child: _MiSlideShow2(accentColor: accentColor))
+        ];
+
+    return Scaffold(
+      body: (isLarge)
+          ? Column(
+            children: children,
+          )
+          : Row(
+            children: children,
+          )
     );
     
+  }
+}
+
+
+class _MiSlideShow1 extends StatelessWidget {
+  const _MiSlideShow1({
+    Key? key,
+    required this.accentColor,
+  }) : super(key: key);
+
+  final Color accentColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      bulletPrimario: 15.0,
+      bulletSecundario: 12.0,
+      colorPrimario: Colors.grey,
+      colorSecundario: accentColor,
+      puntosArriba: false,
+      slides: [
+      SvgPicture.asset('assets/svgs/slide-1.svg' ),
+      SvgPicture.asset('assets/svgs/slide-2.svg' ),
+      SvgPicture.asset('assets/svgs/slide-3.svg' ),
+      SvgPicture.asset('assets/svgs/slide-4.svg' ),
+      SvgPicture.asset('assets/svgs/slide-5.svg' ),
+    ],
+          );
+  }
+}
+
+class _MiSlideShow2 extends StatelessWidget {
+  const _MiSlideShow2({
+    Key? key,
+    required this.accentColor,
+  }) : super(key: key);
+
+  final Color accentColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Slideshow(
+      bulletPrimario: 15.0,
+      bulletSecundario: 12.0,
+      colorPrimario: Colors.grey,
+      colorSecundario: accentColor,
+      puntosArriba: true,
+      slides: [
+      Image.network('https://i.ytimg.com/vi/tshXzKpgZWs/hqdefault.jpg'),
+      Center(child: Text('Table Tennis 4 All', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),)),
+      Image.network('https://tabletennis-reference.com/images/racket/935_1_450.jpg'),
+      ],);
   }
 }
